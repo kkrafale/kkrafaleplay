a6f24b6ba541ee6803dedbf478de44c811fe68b4
kkrafale <rafael.limaa2012@yahoo.com>
2025-04-19 12:35:59 -0300

    Create firebaseconfig.js

---
+++ firebaseconfig.js
@@ -0,0 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCDkcb6Qf5gWANhViBd4ENhOW4ibYxOsKA",
+  authDomain: "kkrafale-lab.firebaseapp.com",
+  databaseURL: "https://kkrafale-lab-default-rtdb.firebaseio.com",
+  projectId: "kkrafale-lab",
+  storageBucket: "kkrafale-lab.firebasestorage.app",
+  messagingSenderId: "207958605538",
+  appId: "1:207958605538:web:9faa6916295617e1b228bb",
+  measurementId: "G-H0Q56BHFYJ"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
