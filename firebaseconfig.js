ce7f51dc7773f5768312366fe053abbf99e1bb88
kkrafale <rafael.limaa2012@yahoo.com>
2025-04-19 12:40:25 -0300

    Update firebaseconfig.js

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,14 +1,28 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCDkcb6Qf5gWANhViBd4ENhOW4ibYxOsKA",
-  authDomain: "kkrafale-lab.firebaseapp.com",
-  databaseURL: "https://kkrafale-lab-default-rtdb.firebaseio.com",
-  projectId: "kkrafale-lab",
-  storageBucket: "kkrafale-lab.firebasestorage.app",
-  messagingSenderId: "207958605538",
-  appId: "1:207958605538:web:9faa6916295617e1b228bb",
-  measurementId: "G-H0Q56BHFYJ"
-};
+<!-- Firebase SDKs -->
+<script type="module">
+  // Importação dos módulos necessários do Firebase v9+
+  import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
+  import { getDatabase } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js';
+  import { getAuth } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js';
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
+  // Configuração do Firebase
+  const firebaseConfig = {
+    apiKey: "AIzaSyCDkcb6Qf5gWANhViBd4ENhOW4ibYxOsKA",
+    authDomain: "kkrafale-lab.firebaseapp.com",
+    databaseURL: "https://kkrafale-lab-default-rtdb.firebaseio.com",
+    projectId: "kkrafale-lab",
+    storageBucket: "kkrafale-lab.firebasestorage.app",
+    messagingSenderId: "207958605538",
+    appId: "1:207958605538:web:9faa6916295617e1b228bb",
+    measurementId: "G-H0Q56BHFYJ"
+  };
+
+  // Inicializando o Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Pegando instâncias de banco de dados e autenticação
+  const db = getDatabase(app);
+  const auth = getAuth(app);
+
+  // Agora você pode usar 'db' e 'auth' em outras partes do seu código
+</script>
